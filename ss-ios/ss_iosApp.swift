ab8615bd1270efe9b68c99877123577b95985774
maxim-bazant <maxim.bazant@outlook.com>
2023-05-08 21:31:19 +0200

    Initial Commit

---
+++ ss-ios/ss_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ss_iosApp.swift
+//  ss-ios
+//
+//  Created by Maxim Bažant on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ss_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
